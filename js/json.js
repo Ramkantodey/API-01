4a6a3b480abc613353e5681ba6aed428c4ccab93
Ramkantodey <ramkantodey@gmail.com>
2024-10-01 20:57:34 +0600

    first commit

---
+++ js/json.js
@@ -0,0 +1,36 @@
+const user = {
+    id: 1,
+    name: 'Ramkanto Dey',
+    job: 'actor',
+};
+// JavaScript Object Notation (JSON)
+const stringified = JSON.stringify(user);
+// console.log(user);
+// console.log(stringified);
+/**
+ * { id: 1, name: 'Ramkanto Dey', job: 'actor' }
+{"id":1,"name":"Ramkanto Dey","job":"actor"}
+ */
+
+
+
+const shop = {
+    owner: 'kanto',
+    address: {
+        street: 'kocukhet voot er goli',
+        city: 'ranbir',
+        country: 'BD',
+    },
+    products: ['laptop', 'Mic', 'monitor', 'keyboard'],
+    revenue: 45000,
+    isOpen: true,
+    isNew: false,
+}
+
+console.log(shop);
+const shopJSON = JSON.stringify(shop);
+console.log(shopJSON);
+const shopObj = JSON.parse(shopJSON);
+console.log(shopObj);
+
+
